e460f2a06a5d192eb7ac3a2a2392fa7822f276aa
Noemi Ostorga <noemiostorga@gmail.com>
2016-08-22 18:49:34 -0700

    fixed 'exceeded amount' error

--- src/ui/teacherHeader.js
+++ src/ui/teacherHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 export default React.createClass({
   render: function () {
     return (
       <div>
-      	<h1>Teacher View</h1>
+      	<h1> Teacher view </h1>
       </div>
     )
   }
