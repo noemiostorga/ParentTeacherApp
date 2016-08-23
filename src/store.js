68aae826729f4ffaa8a35f427f0e8d66f92bc4df
Noemi Ostorga <noemiostorga@gmail.com>
2016-08-22 21:16:41 -0700

    added reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 
 // import reducers here
-
+import reducer from 'reducers/reducer.js'
 // combine reducers
 const reducers = combineReducers({
   // put reducers here
+  	reducer
 })
 
 // crerate the store
